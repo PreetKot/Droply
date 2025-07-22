13e15ef47061d34c40ff5a39eabc864a8131efc7
Preet Kotmire <preetkotmire@Preets-MacBook-Air.local>
2025-07-22 16:30:05 +0530

    Fix middleware authentication error

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,6 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
-
-export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) {
-    const session = await auth();
-    if (!session.userId) {
-      return Response.redirect(new URL('/sign-in', req.url));
-    }
-  }
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
